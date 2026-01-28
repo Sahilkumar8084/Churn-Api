9b8801f1e38e844eebbddef578d14a2189bc5eb2
Sahilkumar8084 <sahilkumarrock8084@gmail.com>
2026-01-28 11:25:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
+import os
 
 app = Flask(__name__) #app object banata hai ye
 
@@ -43,4 +44,5 @@ def predict():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) # Render sets PORT 
+    app.run(host="0.0.0.0", port=port, debug=True)
